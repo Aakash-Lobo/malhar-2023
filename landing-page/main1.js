9f8d1bed46b4792b78330b2049d898910e7935c4
KanielPinto <kaniel.pinto@xaviers.edu.in>
2023-05-20 17:52:48 +0530

    trial

--- landing-page/main1.js
+++ landing-page/main1.js
@@ -14,21 +14,24 @@ var scene,
 function initScene() {
   scene = new THREE.Scene();
 
-  sceneLight = new THREE.DirectionalLight(0xffffff, 0.5);
-  sceneLight.position.set(0, 0, 1);
-  scene.add(sceneLight);
+  // let ambient = new THREE.AmbientLight(0x8f84ad);
+  // scene.add(ambient);
 
-  // portalLight = new THREE.PointLight(0x1d2f51, 50, 600, 1.7);
-  // portalLight.position.set(0, 0, 250);
-  // scene.add(portalLight);
+  // sceneLight = new THREE.DirectionalLight(0xffffff);
+  // sceneLight.position.set(0, 0, 1);
+  // scene.add(sceneLight);
+
+  portalLight = new THREE.PointLight(0x1d2f51, 50, 600, 1.7);
+  portalLight.position.set(0, 0, 250);
+  scene.add(portalLight);
 
   // Mobile camera
   if (window.innerWidth <= 768) {
-    FOV = 90;
+    FOV = 120;
     FAR = 1000;
     // 769px - 1080px screen width camera
   } else {
-    FOV = 70;
+    FOV = 80;
     FAR = 800;
   }
 
@@ -42,27 +45,33 @@ function initScene() {
   scene.add(cam);
 
   renderer = new THREE.WebGLRenderer({
-    antialias: false
+    antialias: false,
   });
-  renderer.setClearColor(0x201b24, 1);
+  renderer.setClearColor(0x000000, 1);
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  let lightPurpleLight = new THREE.PointLight(0xd48fd4, 30, 700, 2);
-  // let lightPurpleLight = new THREE.HemisphereLight(0xd48fd4, 0x201b24, 10);
-  lightPurpleLight.position.set(-100, 100, 100);
-  scene.add(lightPurpleLight);
+  // let directionalLight = new THREE.DirectionalLight(0xfb8500);
+  // directionalLight.position.set(0, 0, 1);
+  // scene.add(directionalLight);
 
-  // let blueLight = new THREE.HemisphereLight(0xa9abdc, 0x201b24, 12);
-  let blueLight = new THREE.PointLight(0xa9abdc, 50, 700, 2);
-  blueLight.position.set(300, 300, 200);
+  let lightPurpleLight = new THREE.PointLight(0xd48fd4, 8, 1100);
+  lightPurpleLight.position.set(-100, -100, 150);
+  scene.add(lightPurpleLight);
+  window.addEventListener("resize", onWindowResize, false);
+
+  let blueLight = new THREE.PointLight(0xa9abdc, 8, 1100);
+  blueLight.position.set(100, 100, 150);
   scene.add(blueLight);
 
-  window.addEventListener("resize", onWindowResize, false);
+  // const bg_loader = new THREE.TextureLoader();
+  // bg_loader.load("assets/eye.webp", function (texture) {
+  //   scene.background = texture;
+  // });
 
   let loader = new THREE.TextureLoader();
 
-  loader.load("./assets/smoke.png", function (texture) {
+  loader.load("./assets/smoke2.png", function (texture) {
     let portalGeo = new THREE.PlaneGeometry(350, 350);
     let portalMaterial = new THREE.MeshLambertMaterial({
       map: texture,
@@ -74,7 +83,7 @@ function initScene() {
       transparent: true,
     });
 
-    for (let p = 325; p > 50; p--) {
+    for (let p = 880; p > 250; p--) {
       let particle = new THREE.Mesh(portalGeo, portalMaterial);
       particle.position.set(
         0.5 * p * Math.cos((4 * p * Math.PI) / 180),
@@ -86,18 +95,18 @@ function initScene() {
       scene.add(particle);
     }
 
-    for (let p = 0; p < 50; p++) {
-      let particle = new THREE.Mesh(smokeGeo, smokeMaterial);
-      particle.position.set(
-        Math.random() * 400 - 200,
-        Math.random() * 400 - 200,
-        25
-      );
-      particle.rotation.z = Math.random()* 360;
-      particle.material.opacity = 0.6;
-      portalParticles.push(particle);
-      scene.add(particle);
-    }
+    // for (let p = 0; p < 40; p++) {
+    //   let particle = new THREE.Mesh(smokeGeo, smokeMaterial);
+    //   particle.position.set(
+    //     Math.random() * 850 - 500,
+    //     Math.random() * 400 - 200,
+    //     25
+    //   );
+    //   particle.rotation.z = Math.random() * 360;
+    //   particle.material.opacity = 0.6;
+    //   portalParticles.push(particle);
+    //   scene.add(particle);
+    // }
     // clock = new THREE.Clock();
 
     update();
@@ -112,22 +121,31 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function animate() {
-  // let delta = clock.getDelta();
-  // portalParticles.forEach((p) => {
-  //   p.rotation.z -= 0.01 * 0.9;
-  // });
-  // smokeParticles.forEach((p) => {
-  //   p.rotation.z -= 0.01 * 0.8;
-  // });
-  // renderer.render(scene, cam);
-  // requestAnimationFrame(animate);
-}
+window.addEventListener("load", () => {
+  console.clear();
+
+  const loading = document.getElementById("loading");
+  loading.classList.add("loaded");
+
+  initScene();
+});
+
+// function animate() {
+//   let delta = clock.getDelta();
+//   portalParticles.forEach((p) => {
+//     p.rotation.z -= 0.01 * 0.9;
+//   });
+//   smokeParticles.forEach((p) => {
+//     p.rotation.z -= 0.01 * 0.8;
+//   });
+//   renderer.render(scene, cam);
+//   requestAnimationFrame(animate);
+// }
 
 let clock = new THREE.Clock();
 let delta = 0;
 // 30 fps
-let interval = 1 / 10;
+let interval = 1 / 30;
 
 function update() {
   requestAnimationFrame(update);
@@ -136,16 +154,17 @@ function update() {
   if (delta > interval) {
     // The draw or time dependent code are here
     portalParticles.forEach((p) => {
-      p.rotation.z -= 0.9 * 0.1;
+      p.rotation.z -= 0.06 * 0.1;
     });
-    smokeParticles.forEach((p) => {
-      p.rotation.z -= 0.00000000001;
-    });
+    // smokeParticles.forEach((p) => {
+    //   p.rotation.z -= 0.00000000001;
+    // });
 
+    if (Math.random() > 0.9) {
+      portalLight.power = 350 + Math.random() * 500;
+    }
     renderer.render(scene, cam);
 
     delta = delta % interval;
   }
 }
-
-initScene();
